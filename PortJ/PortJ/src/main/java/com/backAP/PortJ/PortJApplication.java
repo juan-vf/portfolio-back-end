ff1e88ef3f2b6fb141f4a13f6abe203e96e632a3
unknown <juanvillan777@gmail.com>
2023-03-21 20:36:07 -0300

    commit initializer project

---
+++ PortJ/PortJ/src/main/java/com/backAP/PortJ/PortJApplication.java
@@ -0,0 +1,13 @@
+package com.backAP.PortJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortJApplication.class, args);
+	}
+
+}
---
+++ PortJ/PortJ/src/test/java/com/backAP/PortJ/PortJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backAP.PortJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
